b6c0947e4cff9697ecd30c52dd92c6862194698e
adhya-putta <adhyaputta68@gmail.com>
2025-07-15 09:59:48 -0500

    set up streamlit app.py with basic details of project

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="AI Skincare Recommender", layout="centered")
+
+st.title("AI Skincare Recommender")
+st.write(
+    "Hii! I'm so excited to help you choose the best product for your skin concerns! It's super easy, just select your skin type and concerns to get your personalized skincare product recommendations."
+)
