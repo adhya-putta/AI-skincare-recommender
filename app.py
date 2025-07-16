5919efadde849cf8627ea752e3dacb2e7d4b5bba
adhya-putta <adhyaputta68@gmail.com>
2025-07-16 10:11:35 -0500

    Added two user input select boxes for skin type and skin concerns using Streamlit

--- app.py
+++ app.py
@@ -2,7 +2,23 @@ import streamlit as st
 
 st.set_page_config(page_title="AI Skincare Recommender", layout="centered")
 
-st.title("AI Skincare Recommender")
+st.title("AI skincare recommender")
 st.write(
-    "Hii! I'm so excited to help you choose the best product for your skin concerns! It's super easy, just select your skin type and concerns to get your personalized skincare product recommendations."
+    "hii! i'm so excited to help you choose the best product for your skin concerns! it's super easy, just select your skin type and concerns to get your personalized skincare product recommendations."
 )
+
+skin_type = st.selectbox(
+    "please select your skin type:",
+    ["Oily", "Dry", "Combination", "Sensitive", "Normal"]
+)
+
+
+concerns = st.multiselect(
+    "what are your current skin concerns?",
+    ["Acne", "Redness", "Dryness", "Aging", "Dark spots", "Sensitivity", "Comedones"]
+)
+
+if st.button("get your personalized recommendations"):
+    st.subheader(" based on your input...")
+    st.write(f"Skin type: **{skin_type}**")
+    st.write(f"Concerns: **{', '.join(concerns)}**")
